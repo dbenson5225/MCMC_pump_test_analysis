425ff4fdafdf8c757c8793c37375ae1f0482eddf
David Benson <dbenson@mines.edu>
2025-10-13 10:03:44 -0600

    Add files via upload

---
+++ well CR-27R/bounds.py
@@ -0,0 +1,4 @@
+import numpy as np
+STCPK_guess    = np.array([  0.075,   300,  -43,  4.0,  -1.8])
+lower_bound    = np.array([1.e-3,     1.,  -60,  1.0,  -10.])
+upper_bound    = np.array([  0.5,  2000.,    0,  5.0,    5.])
